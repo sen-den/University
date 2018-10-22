bb33647774c650bafe862151cf9b1a092a13d456
Denis Senchishen <senden1998@gmail.com>
2018-10-23 00:01:49 +0300

    Some more

---
+++ trash/pas/6/6_2_3.pas
@@ -0,0 +1,18 @@
+var
+	a,b,c:double;
+
+function arccos(a:double):double;
+	begin
+		arccos:=arctan(sqrt(1-sqr(cos(a)))/cos(a));
+	end;
+
+begin
+	write('a = ');readln(a);
+	write('b = ');readln(b);
+	write('c = ');readln(c);
+	{cos(ax+b) = c}
+	{x = (acos(c)-b) /a}
+	{Наверное}
+	writeln((arccos(c)-b)/a);
+
+end.
