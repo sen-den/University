99dd461b791c052ed08e10e8f9ed8aedd5cdc221
Denis Senchishen <senden1998@gmail.com>
2018-10-23 00:14:56 +0300

    More pas

---
+++ trash/pas/7/7_3.pas
@@ -0,0 +1,42 @@
+const nOfTrains = 3;
+type
+	Train=record
+		number:integer;
+		st_priznach:string;
+		time_otprav:string;
+		time_v_pyti:string;
+	end;
+var	
+	Trains:array[1..nOfTrains] of Train;
+	i:integer;
+	find_punkt:string;
+	find_numbr:integer;
+
+begin
+	for i:=1 to nOfTrains do begin
+		writeln('');
+		writeln(i,' Train:');
+		write('Number:', '':18);
+		readln(Trains[i].number);
+		write('Stanzia priznachenya:', '':4);
+		readln(Trains[i].st_priznach);
+		write('Time otpravki:', '':11);
+		readln(Trains[i].time_otprav);
+		write('Time v pyti:', '':13);
+		readln(Trains[i].time_v_pyti);
+	end;
+
+	writeln('');
+	write('Number for search: ');
+	readln(find_numbr);
+	write('Punkt for search: ');
+	readln(find_punkt);
+
+
+	for i:=1 to nOfTrains do begin
+		if (Trains[i].number=find_numbr) then
+			writeln(Trains[i].number, ' GOES TO ', Trains[i].st_priznach);
+		if (Trains[i].st_priznach=find_punkt) then
+			writeln(Trains[i].number, ' GOES TO ', Trains[i].st_priznach);
+		end;
+end.
