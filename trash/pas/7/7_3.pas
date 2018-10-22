fcb544af5c51ec24d9c964938f10faca04917f16
Denis Senchishen <senden1998@gmail.com>
2018-10-23 00:23:44 +0300

    More pas

--- trash/pas/7/7_3.pas
+++ trash/pas/7/7_3.pas
@@ -1,6 +1,6 @@
-const nOfTrains = 3;
+const nOfTrains = 3; {Сколько бкдет зеписей}
 type
-	Train=record
+	Train=record {Структура записи из условия}
 		number:integer;
 		st_priznach:string;
 		time_otprav:string;
@@ -9,10 +9,10 @@ type
 var	
 	Trains:array[1..nOfTrains] of Train;
 	i:integer;
-	find_punkt:string;
+	find_punkt:string; {То, по чему будем искать}
 	find_numbr:integer;
 
-begin
+begin {Вводим все}
 	for i:=1 to nOfTrains do begin
 		writeln('');
 		writeln(i,' Train:');
@@ -26,13 +26,14 @@ begin
 		readln(Trains[i].time_v_pyti);
 	end;
 
+	{То, по чему будем искать}
 	writeln('');
 	write('Number for search: ');
 	readln(find_numbr);
 	write('Punkt for search: ');
 	readln(find_punkt);
 
-
+	{Выводим все, что нашли}
 	for i:=1 to nOfTrains do begin
 		if (Trains[i].number=find_numbr) then
 			writeln(Trains[i].number, ' GOES TO ', Trains[i].st_priznach);
